bcaa72a8821c36dd3d2e40cf7c1a895e766f921f
Motor Cycle <toomanymotorcycles06@gmail.com>
2026-03-25 12:32:11 +1300

    Authentication issues are finally fixed! 	modified:   pages/login/login.html 	modified:   pages/login/login_handler.mjs 	modified:   pages/login/style-login.css

--- pages/login/login_handler.mjs
+++ pages/login/login_handler.mjs
@@ -1,21 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
 import { getFirestore, collection as col, doc, addDoc, deleteDoc, getDoc as get, getDocs as getm, query, orderBy, limit, onSnapshot as onSnap, Timestamp, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
-import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getAuth, setPersistence, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA8eRtuze9y-JNH3Qe7kmMlvSBvvR860uU",
-    authDomain: "comp-2025-joshua-kh.firebaseapp.com",
-    projectId: "comp-2025-joshua-kh",
-    storageBucket: "comp-2025-joshua-kh.firebasestorage.app",
-    messagingSenderId: "666152943807",
-    appId: "1:666152943807:web:487145ec86c0e197dfc60a",
-    measurementId: "G-J1FZFCMSRE"
+  apiKey: "AIzaSyCNRUASOXGQabiR8LGBzKP6BDSEEAHdTR8",
+  authDomain: "comp-2025-joshua-kh.firebaseapp.com",
+  projectId: "comp-2025-joshua-kh",
+  storageBucket: "comp-2025-joshua-kh.firebasestorage.app",
+  messagingSenderId: "666152943807",
+  appId: "1:666152943807:web:487145ec86c0e197dfc60a",
+  measurementId: "G-J1FZFCMSRE"
 };
 
 var app, analytics, userAuthService, authProvider, db, initialised = false;
 function init() {
-    consol.info("-------------------------------------\n--- CHAOS DATABASE SOLUTIONS V1.0 ---\n------ COPYRIGHT OF CHAOS INC. ------\n-------------------------------------");
+    console.info("-------------------------------------\n--- CHAOS DATABASE SOLUTIONS V1.0 ---\n------ COPYRIGHT OF CHAOS INC. ------\n-------------------------------------");
     console.log("CDS: Initialising...");
     document.getElementById("loading").removeAttribute("hidden");
     globalThis.user = undefined;
@@ -23,7 +23,7 @@ function init() {
     analytics = getAnalytics(app);
     db = getFirestore(app);
     document.getElementById("loading").setAttribute("hidden",true);
-    document.getElementById("page").removeAttribute("hidden");
+    document.getElementById("page").removeAttribute("style");
     console.log("CDS: Initialisation complete.");
 }
 try {
@@ -31,7 +31,11 @@ try {
 } catch (err) {
     console.error(`-!- CDS FATAL ERROR -!-\nInitialisation FAILED\n${err}`);
     document.getElementById("loading-gif").setAttribute("src","./../../resources/resources_global/warning.png");
+    document.getElementById("loading-gif").setAttribute("width",200);
+    document.getElementById("loading-gif").setAttribute("height",200);
     document.getElementById("loading-message").innerText = "A fatal error has occured, try again later.";
+    document.getElementById("loading-message").setAttribute("style","color:indianred");
+    document.getElementById("fatalerror-info").removeAttribute("hidden");
 };
 
 function toggleButtonState(button,active,textIfActive) {
@@ -83,7 +87,10 @@ function loginWithUserPasswd(email,passwd) {
             case "auth/missing-password":
                errorMessage.innerText = "Please enter your email and password.";
                break;
-            case "auth/operation-not-allowed":
+            case "auth/invalid-credential":
+               errorMessage.innerText = "Incorrect username or password.";
+               break;
+            default:
                errorMessage.innerText = "Internal authentication error, try again later.";
                break;
         };
