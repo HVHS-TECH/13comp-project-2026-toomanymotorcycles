b071f8847e1429ad016cf776a8356199069afb39
Motor Cycle <toomanymotorcycles06@gmail.com>
2026-06-04 13:46:28 +1200

    modified:   lobbySystem/lobby-module.mjs

--- lobbySystem/lobby-module.mjs
+++ lobbySystem/lobby-module.mjs
@@ -6,44 +6,58 @@ class Lobby {
     constructor(gameID, lobbyID, host) {
         if (lobbyID == 0) {this.lobbyID = Math.round(Math.random()^2*10)} else {this.lobbyID = lobbyID}
         this.host = host;
-        this.maxPlayerCount = 2;
+        this.maxPlayerCount = null;
         this.playerList = [];
-        this.joinable = false;
+        this.joinable = null;
         get(doc(db, gameEntries, gameID)).then((snap) => {
             if (snap.exists()) {
                 this.maxPlayerCount = 2;
             } else {
-                error("The requested game does not exist.");
+                console.error("The requested game does not exist.");
                 this = null;
+            }
+            if (this.host == getAuth().currentUser.uid) {
+                this.playerList.push(getAuth().currentUser.uid);
+                this.joinable = true;
             }
         })
         // set up event listener to detect whenever the database data changes
     }
 
-    open() {
-        if (this.host != getAuth().currentUser.uid) {console.warn("Lobbies can only be opened by their host."); return;}
-        this.playerList.push(getAuth().currentUser.uid);
-        this.joinable = true;
+    fallback() {
+        console.error("Fallback system triggered! Resetting lobby...")
     }
 
     join() {
+        // attempts to join lobby
         if (this.playerList.includes(getAuth().currentUser.uid)) {console.warn("You are already in this lobby."); return false;}
         if (!this.joinable) {console.warn("You cannot join this lobby."); return false;}
         if (this.playerList.length >= this.maxPlayerCount) {console.warn("This lobby is full."); return false;}
-        this.playerList.push();
+        this.playerList.push(getAuth().currentUser.uid);
         return true;
     }
 
     leave() {
+        // attempts to leave lobby
+        if (!this.playerList.includes(getAuth().currentUser.uid)) {console.warn("You are not in this lobby."); return false;}
+        this.playerList.splice(this.playerList.findIndex((uid) => {return uid == getAuth().currentUser.uid}),1);
+        return true;
+    }
 
+    lock() {
+        // requests to database to lock lobby, can only be done as host
+    }
+
+    unlock() {
+        // requests to database to unlock lobby, can only be done as host
     }
 
     dataUpdate() {
-
+        // manually triggers event listener
     }
 
     destroy() {
-
+        // requests to database to destroy lobby, can only be done as host
     }
 }
 
