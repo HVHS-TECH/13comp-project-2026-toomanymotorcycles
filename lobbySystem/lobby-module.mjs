8e9f7bc46fb01bb6e80cc6cfce655cfdd658719e
Motor Cycle <toomanymotorcycles06@gmail.com>
2026-06-12 15:17:17 +1200

    modified:   lobbySystem/lobby-module.mjs

--- lobbySystem/lobby-module.mjs
+++ lobbySystem/lobby-module.mjs
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getFirestore, collection as col, doc, getDoc, getDocs as getm, query, orderBy, limit, onSnapshot as onSnap } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
-import { getDatabase, ref, get, onValue, runTransaction } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
+import { getDatabase, ref, get, set, onValue, runTransaction } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
 //I know I'm using both types of database, and I know that this is inefficiency given form. Shut up. Rebuilding my entire architecture would take too long.
@@ -19,37 +19,58 @@ const firebaseConfig = {
   storageBucket: "comp-2025-joshua-kh.firebasestorage.app",
   messagingSenderId: "666152943807",
   appId: "1:666152943807:web:487145ec86c0e197dfc60a",
-  measurementId: "G-J1FZFCMSRE"
+  measurementId: "G-J1FZFCMSRE",
 };
-
-const app = await initializeApp(firebaseConfig);
-const fdb = getFirestore(app);
+const initialised = false;
+const app = initializeApp(firebaseConfig);
+const fsdb = getFirestore(app);
 const rtdb = getDatabase(app);
 console.info("-------------------------------------\n------- CDS LOBBY MODULE V1.0 -------\n------ COPYRIGHT OF CHAOS INC. ------\n-------------------------------------");
 
 class ActiveLobby {
-    constructor(lobbyID) {
-        if (lobbyID == 0) {
-            this.lobbyID = Math.round(Math.random()^2*10)
-        } else {
-            this.lobbyID = lobbyID
-        };
+    static generateCode() {
+        //I stole this from geeksforgeeks.org
+        let result = '';
+        for (let i = 0; i < 5; i++) {
+            result += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+        }
+        return result;
+    }
+    constructor(lobbyID, gameID) {
         this.gameID = gameID;
-        this.host = host;
-        this.maxPlayerCount = max;
-        this.playerList = playerList;
-        this.joinable = joinable;
-        this.data = data;
+        if (lobbyID == 0) {
+            this.lobbyID = this.generateCode();
+            set(ref(rtdb, `/gameLobbies/${this.gameID}/${this.lobbyID}`), {
+            host: getAuth().currentUser.uid,
+            maxPlayerCount: 2,
+            playerList: new Map(),
+            joinable: true,
+            data: {turn:0}
+        })
+        } else {
+            this.lobbyID = lobbyID;
+        };
+        onValue(ref(rtdb, `/gameLobbies/${this.gameID}/${this.lobbyID}`), (lobbyData) => {
+            if (lobbyData.exists()) {
+                this.host = lobbyData.val().host;
+                this.maxPlayerCount = lobbyData.val().max;
+                this.playerList = lobbyData.val().playerList;
+                this.joinable = lobbyData.val().joinable;
+                this.data = lobbyData.val().data;
+            } else {
+                throw new ReferenceError("Invalid lobby.")
+            }
+        });
     }
 }
 
 function getLobbies() {
-
+    
 }
 
-function joinLobby() {
-    // attempts to join lobby
-        
+function joinLobby(lobbyID,gameID) {
+    joinedLobby = new ActiveLobby(lobbyID,gameID);
+    
 }
 
 function leaveLobby() {
@@ -69,6 +90,8 @@ function destroyLobby() {
     // requests to database to destroy lobby, can only be done as host
 }
 
+getLobbies();
+joinLobby("uxaw","owdihwidnw");
 
 //okay, so the solution is a mutation observer, i can set on up on all lobbies that updates all changed values in the database.
 //if i don't sleep, I won't be able to think, so I should probably sleep
